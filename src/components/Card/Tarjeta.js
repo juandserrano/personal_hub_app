17247eecb4d30d18afe2d4af9dcac44e896ef1d5
Juan Diego Serrano <juandserrano@gmail.com>
2020-08-16 14:19:55 -0500

    more

--- src/components/Card/Tarjeta.js
+++ src/components/Card/Tarjeta.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import {Button} from '@material-ui/core'
+import axios from 'axios'
 
 const useStyles = makeStyles({
   root: {
@@ -14,35 +14,74 @@ const useStyles = makeStyles({
     maxWidth: 345,
     minWidth: 250,
     borderRadius: 30,
+    padding: 0
 
   },
   media: {
     height: 140,
   },
+  contenido: {
+    minHeight: 300
+  },
+  botonsaldo: "blue"
 });
 
 export default function Tarjeta({ info: { titulo, contenido, imagen, link } }) {
   const classes = useStyles();
 
+  const soloSaldoFiducuenta = async (e) => {
+    e.stopPropagation();
+   const { data } = await axios.get('http://127.0.0.1:3000/api/fiducuenta')
+    alert(data.saldo)
+  }
+
+  const switchOnClick = (titulo) => {
+    if(titulo === 'Fiducuenta'){
+      executeFiducuenta();
+    } else if (titulo === 'Keto Final'){
+      executeKetoFinal();
+    } else if (titulo === 'Nos vamos pa Ottawa'){
+      executeOttawa();
+    } else {
+      return null;
+    }
+  }
+
+  const executeFiducuenta = () => {
+    console.log('executing fiducuenta')
+  }
+
+  const executeKetoFinal = () => {
+    console.log('executing keto')
+  }
+  
+  const executeOttawa = () => {
+    console.log('executing ottawa')
+  }
+
   return (
     <Card className={classes.root}>
-      <CardActionArea href={link}>
+      <CardActionArea className={classes.contenido} onClick={() => switchOnClick(titulo)}>
         <CardMedia
           className={classes.media}
           image={imagen}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="h2">
-            {titulo}
+            {titulo}{titulo === 'Fiducuenta' ? (<Button color='#primary' onClick={soloSaldoFiducuenta}>Ver Saldo</Button>) : null}
           </Typography>
+          
           <Typography variant="body2" color="textSecondary" component="p">
             {contenido}
           </Typography>
         </CardContent>
       </CardActionArea>
-      <CardActions>
-        
-      </CardActions>
-    </Card>
+     </Card>
   );
+
+    
+
+    
+  
+
 }
